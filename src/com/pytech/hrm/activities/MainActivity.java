4a82aec6dc306eff84251d282691727f844ea9f4
pytarng <pytarng@gmail.com>
2014-09-05 00:52:00 +0800

    new main layout

--- src/com/pytech/hrm/activities/MainActivity.java
+++ src/com/pytech/hrm/activities/MainActivity.java
@@ -4,30 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnLongClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.pytech.hrm.R;
-import com.pytech.hrm.util.constants.HRM;
+import com.pytech.hrm.adapters.ItemAdapter;
+import com.pytech.hrm.models.Item;
+import com.pytech.hrm.util.constants.Colors;
 
 public class MainActivity extends Activity {
 
+	private ItemAdapter itemAdapter;
+	private List<Item> itemList;
+	
 	private ListView itemListView;
-	private TextView titleTextView;
-
-	private ArrayAdapter<String> adapter;
-	private List<String> dataList = new ArrayList<String>();
+	private MenuItem add_item, search_item, revert_item, share_item, delete_item;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,53 +71,53 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(resultCode == Activity.RESULT_OK) {
-			String titleText = data.getStringExtra(HRM.KEY_TITLE_TEXT);
-			if(titleText != null && !titleText.trim().equals("")) {
-				this.dataList.add(titleText);
-			}
-			this.adapter.notifyDataSetChanged();
-		}
+//		if(resultCode == Activity.RESULT_OK) {
+//			String titleText = data.getStringExtra(HRM.KEY_TITLE_TEXT);
+//			if(titleText != null && !titleText.trim().equals("")) {
+//				this.dataList.add(titleText);
+//			}
+//			this.adapter.notifyDataSetChanged();
+//		}
 	}
 
 	protected void processViews() {
 		this.itemListView = (ListView) this.findViewById(R.id.record_list);
-		this.titleTextView = (TextView) this.findViewById(R.id.main_view_title);
 	}
 
 	protected void processControllers() {
-		// Long click event handler for title text view.
-		OnLongClickListener titleOnLongClickListener = new OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-				dialog.setTitle(R.string.app_name).setMessage(R.string.about).show();
-				return false;
-			}
-		};
-		this.titleTextView.setOnLongClickListener(titleOnLongClickListener);
-
-		// Click event handler for record list view.
-		OnItemLongClickListener onItemLongClickListener = new OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-				String content = (String) parent.getItemAtPosition(position);
-				Toast.makeText(MainActivity.this, content, Toast.LENGTH_SHORT).show();
-				return false;
-			}
-		};
-		this.itemListView.setOnItemLongClickListener(onItemLongClickListener);
+//		// Long click event handler for title text view.
+//		OnLongClickListener titleOnLongClickListener = new OnLongClickListener() {
+//			@Override
+//			public boolean onLongClick(View v) {
+//				AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+//				dialog.setTitle(R.string.app_name).setMessage(R.string.about).show();
+//				return false;
+//			}
+//		};
+//		this.titleTextView.setOnLongClickListener(titleOnLongClickListener);
+//
+//		// Click event handler for record list view.
+//		OnItemLongClickListener onItemLongClickListener = new OnItemLongClickListener() {
+//			@Override
+//			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//				String content = (String) parent.getItemAtPosition(position);
+//				Toast.makeText(MainActivity.this, content, Toast.LENGTH_SHORT).show();
+//				return false;
+//			}
+//		};
+//		this.itemListView.setOnItemLongClickListener(onItemLongClickListener);
 	}
 
 	protected void processDatas() {
 		// Generate initial data and adapter.
-		this.dataList.add("Mission 1");
-		this.dataList.add("Mission 2");
-		this.dataList.add("Mission 3");
-		int layoutId = android.R.layout.simple_list_item_1;
-
-		// Put data into list view through adapter.
-		this.adapter = new ArrayAdapter<String>(this, layoutId, dataList);
-		this.itemListView.setAdapter(adapter);
+		this.itemList = new ArrayList<Item>();
+		long now = System.currentTimeMillis();
+		this.itemList.add(new Item(1, now, Colors.RED, "Mission 1", "Scene: xxx.ma; Frames: 1-10", "", 0, 0, 0));
+		this.itemList.add(new Item(2, now, Colors.BLUE, "Mission 2", "Scene: abc.blend; Frames: 1-20", "", 0, 0, 0));
+		this.itemList.add(new Item(3, now, Colors.GREEN, "Mission 3", "Scene: test.mb; Frames: 30-50", "", 0, 0, 0));
+	 
+		// Put item data into list view through self-defined adapter.
+		this.itemAdapter = new ItemAdapter(this, R.layout.item, this.itemList);
+		this.itemListView.setAdapter(this.itemAdapter);
 	}
 }
