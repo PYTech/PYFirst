b668d4f96dcfe3859b9340090846724dc5e0933b
pytarng <pytarng@gmail.com>
2014-09-07 18:14:30 +0800

    minor modify

--- src/com/pytech/first/activities/MainActivity.java
+++ src/com/pytech/first/activities/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		this.setTitle(StringUtils.EMPTY);
 
 		// Initialization.
 		this.processViews();
