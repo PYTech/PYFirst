4328479d197be25e5e0fc23cfb8df16977771c4a
pytarng <pytarng@gmail.com>
2014-09-16 09:09:05 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/com/pytech/first/adapters/ItemAdapter.java

--- src/com/pytech/first/activities/PreferenceActivity.java
+++ src/com/pytech/first/activities/PreferenceActivity.java
@@ -16,7 +16,6 @@ public class PreferenceActivity extends android.preference.PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// 指定使用的設定畫面配置資源
 		addPreferencesFromResource(R.xml.mypreference);
 
 		this.defaultColor = (Preference) this.findPreference(this.getString(R.string.PREF_KEY_COLOR));
@@ -26,11 +25,9 @@ public class PreferenceActivity extends android.preference.PreferenceActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		// 讀取設定的預設顏色
 		int color = this.sharedPreferences.getInt(this.getString(R.string.PREF_KEY_COLOR), FIRST.UNKNOWN);
 
 		if(color != FIRST.UNKNOWN) {
-			// 設定顏色說明
 			this.defaultColor.setSummary(getString(R.string.default_color_summary) + ": " + Colors.fromColorId(color));
 		}
 	}
